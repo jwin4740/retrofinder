1f65907d30dc599530322b2c721d9da28800516b
James <jwin4740@gmail.com>
2017-03-12 20:56:27 -0400

    still working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,16 +21,7 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-    console.log(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-    console.log(waitListData);
-  });
-
+  
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
     console.log(friendsData);
@@ -43,19 +34,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
 
-  });
 
     app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
@@ -71,11 +50,4 @@ module.exports = function(app) {
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,13 +15,7 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/tables.html"));
-  });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/reserve.html"));
-  });
 
     app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
