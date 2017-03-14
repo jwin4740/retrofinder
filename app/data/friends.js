01a843599dae4d5c970a791c506c847998934284
James <jwin4740@gmail.com>
2017-03-14 09:43:15 -0400

    resorting

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,59 +7,133 @@
 
 var friends = [{
     friendName: "James",
+    ageRange: 0,
     gender: "male",
     friendEmail: "james@example.com",
     friendUserName: "jwin474089",
     phoneNumber: "630-715-7982",
     shortBio: "",
-    surveyResults: [{
-        raw: [1, 4, 6],
-        average: 0,
-        type: "leisure"
-    }, {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "morals"
-    }, {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "interests"
-    },  {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "superficiality"
-    }],
+    leisureAverage: 5,
+    moralsAverage: 8,
+    interestsAverage: 6,
+    superficialityAverage: 3,
+    compatibilityScore: 0,
     profile: "asdfasdfasdfasdfadsf",
     imageUrl: "http://bestfloridatalents.com/wp-content/uploads/2010/07/RJtieheadshot.jpg"
 
 }, {
     friendName: "Steve",
+    ageRange: 0,
     gender: "male",
     friendEmail: "steve@example.com",
     friendUserName: "scubasteve",
     phoneNumber: "630-968-0689",
     shortBio: "",
-    surveyResults: [{
-        raw: [1, 4, 6],
-        average: 0,
-        type: "leisure"
-    }, {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "morals"
-    }, {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "interests"
-    }, {
-        raw: [3, 4, 2],
-        average: 0,
-        type: "superficiality"
-    }],
+    leisureAverage: 8,
+    moralsAverage: 3,
+    interestsAverage: 6,
+    superficialityAverage: 8,
+    compatibilityScore: 0,
     profile: "asfasfafsfadff",
     imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
 
-}];
+}, {
+    friendName: "Ryan",
+    ageRange: 0,
+    gender: "male",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 5,
+    moralsAverage: 5,
+    interestsAverage: 5,
+    superficialityAverage: 5,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, {
+    friendName: "Fred",
+    ageRange: 0,
+    gender: "male",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 2,
+    moralsAverage: 2,
+    interestsAverage: 6,
+    superficialityAverage: 2,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, {
+    friendName: "Sarah",
+    ageRange: 0,
+    gender: "female",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 4,
+    moralsAverage: 2,
+    interestsAverage: 6,
+    superficialityAverage: 7,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, {
+    friendName: "Angelica",
+    ageRange: 0,
+    gender: "female",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 7,
+    moralsAverage: 6,
+    interestsAverage: 8,
+    superficialityAverage: 4,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, {
+    friendName: "Eliza",
+    ageRange: 0,
+    gender: "female",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 6,
+    moralsAverage: 3,
+    interestsAverage: 3,
+    superficialityAverage: 1,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, {
+    friendName: "Peggy",
+    ageRange: 0,
+    gender: "female",
+    friendEmail: "steve@example.com",
+    friendUserName: "scubasteve",
+    phoneNumber: "630-968-0689",
+    shortBio: "",
+    leisureAverage: 5,
+    moralsAverage: 3,
+    interestsAverage: 3,
+    superficialityAverage: 9,
+    compatibilityScore: 0,
+    profile: "asfasfafsfadff",
+    imageUrl: "https://i0.wp.com/www.whatgivesphilanthropy.com/wp-content/uploads/2016/05/ZH_Headshot.jpg?resize=200%2C200&ssl=1"
+
+}, ];
 
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friends;
